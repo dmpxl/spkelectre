eda624b3db0ae795a6c7666b2d3c86169fa442f9
dmpxl <104197944+dmpxl@users.noreply.github.com>
2022-04-22 16:27:38 +0700

    Progress 1

---
+++ script.js
@@ -0,0 +1,46 @@
+var tombolMenu = document.getElementsByClassName('tombol-menu')[0];
+var menu = document.getElementsByClassName('menu')[0];
+
+tombolMenu.onclick = function() {
+    menu.classList.toggle('active');
+}
+
+menu.onclick = function() {
+    menu.classList.toggle('active');
+
+    const container = document.querySelector(".container"),
+    pwShowHide = document.querySelectorAll(".showHidePw"),
+    pwFields = document.querySelectorAll(".password"),
+    signUp = document.querySelector(".signup-link"),
+    login = document.querySelector(".login-link");
+
+  //   js code to show/hide password and change icon
+  pwShowHide.forEach(eyeIcon =>{
+      eyeIcon.addEventListener("click", ()=>{
+          pwFields.forEach(pwField =>{
+              if(pwField.type ==="password"){
+                  pwField.type = "text";
+
+                  pwShowHide.forEach(icon =>{
+                      icon.classList.replace("uil-eye-slash", "uil-eye");
+                  })
+              }else{
+                  pwField.type = "password";
+
+                  pwShowHide.forEach(icon =>{
+                      icon.classList.replace("uil-eye", "uil-eye-slash");
+                  })
+              }
+          }) 
+      })
+  })
+
+  // js code to appear signup and login form
+  signUp.addEventListener("click", ( )=>{
+      container.classList.add("active");
+  });
+  login.addEventListener("click", ( )=>{
+      container.classList.remove("active");
+  });
+
+}
